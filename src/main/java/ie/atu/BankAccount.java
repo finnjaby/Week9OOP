ac1b1d58911ecbae07a7520a01d91dbd02a2dd0e
FinnjaBayer <g00470359@atu.ie>
2025-11-19 16:43:23 +0000

    added getter AccNo

--- src/main/java/ie/atu/BankAccount.java
+++ src/main/java/ie/atu/BankAccount.java
@@ -1,5 +1,15 @@
 package ie.atu;
 
 public class BankAccount {
+private String accNo;
+private String name;
+private double balance;
 
+    public BankAccount(String accNo, String name, double balance) {
+
+    }
+
+    public String getAccNo() {
+        return accNo;
+    }
 }
